d31eba9ff8c269d4477ab3b8b85441ce0888ae46
Noa CUCCU <noacuccu@wifi-eduroam-interne-172-29-94-215.wifi.unistra.fr>
2025-01-29 17:52:21 +0100

    Message descriptif de la modification

--- include/planete.php
+++ include/planete.php
@@ -1,113 +1,155 @@
 <?php
-// Planète 1 : Tatooine
 $planete1 = [
-    "nom" => "tatooine",
+    "nom" => "Tatooine",
     "image" => "./images/planete/tatoo.png",
     "description" => "Planète désertique sous deux soleils, recouverte de dunes de sable.",
     "role" => "Lieu de naissance d'Anakin Skywalker et résidence de Luke Skywalker.",
     "apparitions" => "Trilogie originale, préquelles, séries comme The Mandalorian.",
     "climat" => "Aride et chaud",
-    "habitants_principaux" => "Humains, Jawas, Tuskens"
+    "habitants_principaux" => "Humains, Jawas, Tuskens",
+    "type" => "Désertique",
+    "coordonnees" => "Région des Territoires Extérieurs",
+    "gravite" => "Standard (1.0G)",
+    "langues" => "Galactique Basique, Jawaese",
+    "lien" => "https://fr.wikipedia.org/wiki/Tatooine"
 ];
 
-// Planète 2 : Coruscant
+
 $planete2 = [
-    "nom" => "coruscant",
+    "nom" => "Coruscant",
     "image" => "./images/planete/corus.png",
     "description" => "Planète-cité, siège du Sénat Galactique et de l'Ordre Jedi.",
     "role" => "Centre politique de la République et de l'Empire.",
     "apparitions" => "Préquelles, The Clone Wars.",
     "climat" => "Urbanisé, zones tempérées",
-    "habitants_principaux" => "Humains, espèces variées"
+    "habitants_principaux" => "Humains, espèces variées",
+    "type" => "Urbaine",
+    "coordonnees" => "Noyau galactique",
+    "gravite" => "Standard (1.0G)",
+    "langues" => "Galactique Basique",
+    "lien" => "https://fr.wikipedia.org/wiki/Coruscant"
 ];
 
-// Planète 3 : Naboo
 $planete3 = [
-    "nom" => "naboo",
+    "nom" => "Naboo",
     "image" => "./images/planete/naboo.webp",
     "description" => "Planète luxuriante avec des prairies, des lacs, et des cités sous-marines.",
     "role" => "Planète natale de Padmé Amidala et Palpatine.",
     "apparitions" => "La Menace Fantôme.",
     "climat" => "Tempéré",
-    "habitants_principaux" => "Naboo, Gungans"
+    "habitants_principaux" => "Naboo, Gungans",
+    "type" => "Terrestre",
+    "coordonnees" => "Région de la Bordure Médiane",
+    "gravite" => "Standard (1.0G)",
+    "langues" => "Galactique Basique, Gungan",
+    "lien" => "https://fr.wikipedia.org/wiki/Naboo"
 ];
 
-// Planète 4 : Hoth
 $planete4 = [
-    "nom" => "hoth",
+    "nom" => "Hoth",
     "image" => "./images/planete/hoth.png",
     "description" => "Planète glaciale recouverte de neige et de glace.",
     "role" => "Base de l'Alliance Rebelle dans L'Empire Contre-Attaque.",
     "apparitions" => "Star Wars: Episode V.",
     "climat" => "Glacial",
-    "habitants_principaux" => "Wampas, Tauntauns"
+    "habitants_principaux" => "Wampas, Tauntauns",
+    "type" => "Glaciaire",
+    "coordonnees" => "Région de la Bordure Extérieure",
+    "gravite" => "Légèrement inférieure à la norme",
+    "langues" => "Galactique Basique",
+    "lien" => "https://fr.wikipedia.org/wiki/Hoth_(Star_Wars)"
 ];
 
-// Planète 5 : Endor
 $planete5 = [
-    "nom" => "endor",
+    "nom" => "Endor",
     "image" => "./images/planete/endor.png",
     "description" => "Lune boisée habitée par les Ewoks.",
     "role" => "Lieu de la bataille finale contre l'Empire.",
     "apparitions" => "Le Retour du Jedi.",
     "climat" => "Tempéré et forestier",
-    "habitants_principaux" => "Ewoks"
+    "habitants_principaux" => "Ewoks",
+    "type" => "Forestière",
+    "coordonnees" => "Bordure Extérieure",
+    "gravite" => "Légèrement inférieure à la norme",
+    "langues" => "Ewokese, Galactique Basique",
+    "lien" => "https://fr.wikipedia.org/wiki/Endor_(Star_Wars)"
 ];
 
-// Planète 6 : Dagobah
 $planete6 = [
-    "nom" => "dagobah",
+    "nom" => "Dagobah",
     "image" => "./images/planete/dago.png",
     "description" => "Planète marécageuse et isolée.",
     "role" => "Refuge de Yoda et lieu d'entraînement de Luke Skywalker.",
     "apparitions" => "L'Empire Contre-Attaque, Le Retour du Jedi.",
     "climat" => "Humide et marécageux",
-    "habitants_principaux" => "Créatures sauvages"
+    "habitants_principaux" => "Créatures sauvages",
+    "type" => "Marécageuse",
+    "coordonnees" => "Bordure Extérieure",
+    "gravite" => "Normale",
+    "langues" => "Galactique Basique",
+    "lien" => "https://fr.wikipedia.org/wiki/Dagobah"
 ];
 
-// Planète 7 : Mustafar
 $planete7 = [
-    "nom" => "mustafar",
+    "nom" => "Mustafar",
     "image" => "./images/planete/musta.png",
     "description" => "Planète volcanique recouverte de lave.",
     "role" => "Lieu du duel épique entre Obi-Wan et Anakin, repaire de Dark Vador.",
     "apparitions" => "La Revanche des Sith, Rogue One.",
     "climat" => "Volcanique",
-    "habitants_principaux" => "Mustafariens"
+    "habitants_principaux" => "Mustafariens",
+    "type" => "Volcanique",
+    "coordonnees" => "Bordure Extérieure",
+    "gravite" => "Normale",
+    "langues" => "Galactique Basique",
+    "lien" => "https://fr.wikipedia.org/wiki/Mustafar"
 ];
 
-// Planète 8 : Kashyyyk
 $planete8 = [
-    "nom" => "kashyyyk",
+    "nom" => "Kashyyyk",
     "image" => "./images/planete/kashy.webp",
     "description" => "Planète couverte de jungles et forêts immenses.",
     "role" => "Planète natale de Chewbacca, lieu de bataille des Guerres des Clones.",
     "apparitions" => "La Revanche des Sith, The Clone Wars.",
     "climat" => "Tropical",
-    "habitants_principaux" => "Wookiees"
+    "habitants_principaux" => "Wookiees",
+    "type" => "Jungle",
+    "coordonnees" => "Bordure Médiane",
+    "gravite" => "Normale",
+    "langues" => "Shyriiwook, Galactique Basique",
+    "lien" => "https://fr.wikipedia.org/wiki/Kashyyyk"
 ];
 
-// Planète 9 : Jakku
 $planete9 = [
-    "nom" => "jakku",
+    "nom" => "Jakku",
     "image" => "./images/planete/jakku.png",
     "description" => "Planète désertique jonchée de vaisseaux écrasés.",
     "role" => "Lieu de résidence de Rey, théâtre de grandes batailles.",
     "apparitions" => "Le Réveil de la Force.",
     "climat" => "Aride et chaud",
-    "habitants_principaux" => "Humains"
+    "habitants_principaux" => "Humains",
+    "type" => "Désertique",
+    "coordonnees" => "Bordure Extérieure",
+    "gravite" => "Normale",
+    "langues" => "Galactique Basique",
+    "lien" => "https://fr.wikipedia.org/wiki/Jakku"
 ];
 
-// Planète 10 : Alderaan
 $planete10 = [
-    "nom" => "alderaan",
+    "nom" => "Alderaan",
     "image" => "./images/planete/alde.webp",
     "description" => "Planète pacifique, célèbre pour sa beauté naturelle.",
     "role" => "Planète natale de Leia, détruite par l'Étoile de la Mort.",
     "apparitions" => "Mentionnée dans plusieurs films, détruite dans Un Nouvel Espoir.",
     "climat" => "Tempéré",
-    "habitants_principaux" => "Humains"
+    "habitants_principaux" => "Humains",
+    "type" => "Terrestre",
+    "coordonnees" => "Noyau Galactique",
+    "gravite" => "Normale",
+    "langues" => "Galactique Basique",
+    "lien" => "https://fr.wikipedia.org/wiki/Alderaan"
 ];
+
 
 $planetes = [$planete1, $planete2, $planete3, $planete4, $planete5, $planete6, $planete7, $planete8, $planete9, $planete10];
 ?>
--- include/vaisseaux.php
+++ include/vaisseaux.php
@@ -7,8 +7,14 @@ $vaisseau1 = [
     "role" => "Utilisé pour des missions de contrebande et des batailles clés.",
     "apparitions" => "Présent dans toutes les trilogies.",
     "capacités" => "Rapide, très manœuvrable et équipé de canons laser puissants.",
-    "point_faible" => "Dépendance aux modifications constantes pour fonctionner correctement."
+    "point_faible" => "Dépendance aux modifications constantes pour fonctionner correctement.",
+    "fabricant" => "Corellian Engineering Corporation", 
+    "taille" => "34.75 mètres", 
+    "vitesse_max" => "1 200 km/h dans l'atmosphère",
+    "couleur" => "bleu", 
+    "lien" => "https://fr.wikipedia.org/wiki/Faucon_Millenium" 
 ];
+
 
 // 2. X-Wing
 $vaisseau2 = [
@@ -18,7 +24,12 @@ $vaisseau2 = [
     "role" => "Essentiel dans la destruction des Étoiles de la Mort.",
     "apparitions" => "Trilogie originale, trilogie suite.",
     "capacités" => "Polyvalent avec des torpilles à protons et des boucliers défensifs.",
-    "point_faible" => "Moins rapide et fragile face à des TIE Fighters en grand nombre."
+    "point_faible" => "Moins rapide et fragile face à des TIE Fighters en grand nombre.",
+    "fabricant" => "Incom Corporation",
+    "taille" => "12,5 mètres",
+    "vitesse_max" => "1 050 km/h dans l'atmosphère",
+    "couleur" => "bleu",
+    "lien" => "https://fr.wikipedia.org/wiki/X-wing"
 ];
 
 // 3. TIE Fighter
@@ -29,7 +40,12 @@ $vaisseau3 = [
     "role" => "Utilisé pour des combats spatiaux contre les rebelles.",
     "apparitions" => "Tous les films liés à l'Empire.",
     "capacités" => "Agile et rapide, équipé de canons laser.",
-    "point_faible" => "Absence de boucliers et de systèmes de survie."
+    "point_faible" => "Absence de boucliers et de systèmes de survie.",
+    "fabricant" => "Sienar Fleet Systems",
+    "taille" => "6,3 mètres",
+    "vitesse_max" => "1 200 km/h dans l'atmosphère",
+    "couleur" => "rouge",
+    "lien" => "https://fr.wikipedia.org/wiki/TIE_fighter"
 ];
 
 // 4. Slave I
@@ -40,7 +56,12 @@ $vaisseau4 = [
     "role" => "Utilisé pour la chasse à prime, avec une technologie avancée et des armes puissantes.",
     "apparitions" => "L'Attaque des Clones, L'Empire Contre-Attaque, Le Livre de Boba Fett.",
     "capacités" => "Équipé d'armes avancées comme des bombes soniques.",
-    "point_faible" => "Manque de vitesse face à certains chasseurs rapides."
+    "point_faible" => "Manque de vitesse face à certains chasseurs rapides.",
+    "fabricant" => "Kuat Systems Engineering",
+    "taille" => "21,5 mètres",
+    "vitesse_max" => "1 000 km/h dans l'atmosphère",
+    "couleur" => "rouge",
+    "lien" => "https://fr.wikipedia.org/wiki/Slave_I"
 ];
 
 // 5. Star Destroyer
@@ -51,7 +72,12 @@ $vaisseau5 = [
     "role" => "Utilisé pour dominer les batailles spatiales et comme symbole de l'Empire.",
     "apparitions" => "Présent dans toutes les trilogies et Rogue One.",
     "capacités" => "Capable de transporter des troupes, véhicules et chasseurs TIE.",
-    "point_faible" => "Sa taille le rend vulnérable aux attaques ciblées sur ses systèmes vitaux."
+    "point_faible" => "Sa taille le rend vulnérable aux attaques ciblées sur ses systèmes vitaux.",
+    "fabricant" => "Kuat Drive Yards",
+    "taille" => "1 600 mètres",
+    "vitesse_max" => "975 km/h dans l'atmosphère",
+    "couleur" => "rouge",
+    "lien" => "https://fr.wikipedia.org/wiki/Star_Destroyer"
 ];
 
 // 6. Tantive IV
@@ -62,7 +88,12 @@ $vaisseau6 = [
     "role" => "Utilisé pour transporter les plans de l'Étoile de la Mort dans Un Nouvel Espoir.",
     "apparitions" => "Un Nouvel Espoir, Rogue One.",
     "capacités" => "Vitesse élevée pour un transport diplomatique.",
-    "point_faible" => "Peu équipé pour le combat spatial prolongé."
+    "point_faible" => "Peu équipé pour le combat spatial prolongé.",
+    "fabricant" => "Corellian Engineering Corporation",
+    "taille" => "126,68 mètres",
+    "vitesse_max" => "1 200 km/h dans l'atmosphère",
+    "couleur" => "bleu",
+    "lien" => "https://fr.wikipedia.org/wiki/Tantive_IV"
 ];
 
 // 7. Étoile Noire
@@ -73,7 +104,12 @@ $vaisseau7 = [
     "role" => "Super-arme de l'Empire Galactique, utilisée pour instaurer la peur et le contrôle.",
     "apparitions" => "Un Nouvel Espoir (Destruction 1), Le Retour du Jedi (Destruction 2), Rogue One (Origine).",
     "capacités" => "Destruction planétaire, capacité de transport de troupes et de chasseurs TIE.",
-    "point_faible" => "Conduit d'évacuation thermique vulnérable (version 1), générateur de bouclier (version 2)."
+    "point_faible" => "Conduit d'évacuation thermique vulnérable (version 1), générateur de bouclier (version 2).",
+    "fabricant" => "Empire Galactique",
+    "taille" => "160 kilomètres de diamètre",
+    "vitesse_max" => "Stationnaire",
+    "couleur" => "rouge",
+    "lien" => "https://fr.wikipedia.org/wiki/%C3%89toile_de_la_mort"
 ];
 
 // 8. Base Starkiller
@@ -84,8 +120,14 @@ $vaisseau8 = [
     "role" => "Instrument de terreur du Premier Ordre, utilisée pour éliminer la Nouvelle République.",
     "apparitions" => "Star Wars: Le Réveil de la Force.",
     "capacités" => "Destruction de systèmes stellaires entiers.",
-    "point_faible" => "Instabilité du réacteur principal, exploité par la Résistance."
+    "point_faible" => "Instabilité du réacteur principal, exploité par la Résistance.",
+    "fabricant" => "Premier Ordre",
+    "taille" => "Taille d'une planète",
+    "vitesse_max" => "Stationnaire",
+    "couleur" => "rouge",
+    "lien" => "https://fr.wikipedia.org/wiki/Starkiller_(Star_Wars)"
 ];
+
 
 $vaisseaux = [$vaisseau1, $vaisseau2, $vaisseau3, $vaisseau4, $vaisseau5, $vaisseau6, $vaisseau7, $vaisseau8];
 ?>
